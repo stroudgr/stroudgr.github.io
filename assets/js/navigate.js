978af2757e149b25cb7605d76974747fb0eb5d04
stroudgr <graemerobinsonstroud@gmail.com>
2022-12-09 23:23:41 -0500

    Added resume to home page.

--- assets/js/navigate.js
+++ assets/js/navigate.js
@@ -41,7 +41,7 @@ $(window).scroll( () => {
 
     var emailPos = $('#email').position().top;                                      
     if (currentScrollPos <= prevScrollpos - 1 || currentScrollPos >= emailPos - 1000) {
-      console.log("TRUE");
+      //console.log("TRUE");
       //document.getElementById("navbar").style.top = "0";
       $("#bottomNavbar").css("bottom", "0");
     } else if (currentScrollPos > prevScrollpos+20) {
@@ -49,8 +49,8 @@ $(window).scroll( () => {
 
       //console.log($(document).scrollTop());
       
-      console.log(currentScrollPos); 
-      console.log(emailPos - 1000);
+     // console.log(currentScrollPos); 
+      //console.log(emailPos - 1000);
 
       //console.log($(this).scrollTop() >= $('#target_element').position().top);
 
